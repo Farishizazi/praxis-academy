cb7441840e2bbe48861ba30e3e9bfe9e3b8d18ef
Faris Hizazi <faris.hizazi@mail.ugm.ac.id>
2022-08-19 14:19:05 +0700

    python

---
+++ Python/Getting started - pip.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Common Tasks 
+
+# **Meng-Update pip**
+
+# In[4]:
+
+
+pip install --upgrade pip
+
+
+# **meng-Install paket sampleproject**
+
+# In[2]:
+
+
+pip install sampleproject
+
+
+# **Meng-Install paket dari GITHUB**
+
+# In[1]:
+
+
+pip install git+https://github.com/pypa/sampleproject.git@main
+
+
+# **Meng-Install paket dari File Terdistribusi**
+
+# In[2]:
+
+
+pip install sampleproject-1.0.tar.gz
+
+
+# In[3]:
+
+
+pip install sampleproject-1.0-py3-none-any.whl
+
+
+# **Meng-Install beberapa paket dari File yang Diperlukan**
+
+# In[1]:
+
+
+pip install -r requirements.txt
+
+
+# **Meng-Upgrade paket**
+
+# In[2]:
+
+
+pip install --upgrade sampleproject
+
+
+# **Meng-Uninstall paket**
+
+# In[ ]:
+
+
+pip uninstall sampleproject
+
